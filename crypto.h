6595a85ef220e5fa03cf9bdf3a002c8616e23acf
Adam <adam@xyzdigital.com>
2014-10-10 09:49:11 -0700

    Add RSA crypto functions

---
+++ crypto.h
@@ -0,0 +1,9 @@
+#ifndef CRYPTO_H_
+#define CRYPTO_H_
+
+int public_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted);
+int public_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted);
+int private_encrypt(unsigned char * data,int data_len,unsigned char * key, unsigned char *encrypted);
+int private_decrypt(unsigned char * enc_data,int data_len,unsigned char * key, unsigned char *decrypted);
+
+#endif
